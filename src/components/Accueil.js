7bb719e874cf2b9ebd6eb7bdf9a2dfd393a12c97
MioraRatsiresy <miora.ratsiresy@gmail.com>
2023-01-29 12:33:14 +0300

    fiche

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -10,14 +10,17 @@ import Row from 'react-bootstrap/Row';
 import Alert from 'react-bootstrap/Alert';
 import Modal from 'react-bootstrap/Modal';
 import vente from "../image/vente-enchere.jpg";
+import imgvente from "../image/vente.svg";
 import Button from 'react-bootstrap/Button';
-
+import { MDBCol, MDBContainer, MDBRow, MDBCard, MDBCardText, MDBCardBody, MDBCardImage, MDBBtn, MDBTypography, MDBIcon } from 'mdb-react-ui-kit';
 
 const Accueil = () => {
   const navigate = useNavigate();
   const [enchere, setEnchere] = useState(null);
+  const [fiche, setFiche] = useState(null);
   const [isopen, setIsOpen] = useState(false);
   const [encherir, setRencherir] = useState(false);
+  const [voirfiche, setVoirFiche] = useState(false);
   const [etat, setEtat] = useState(0);
   const [enchereid, setId] = useState(0);
   const [montantmax, setMontant] = useState(0);
@@ -101,8 +104,8 @@ const Accueil = () => {
   }
   function ValiderRencherir() {
     axios.post("http://localhost:4444/miser/" + enchereid + "/" + sessionStorage.getItem("TokenUser"), null, { params: { "idclient": sessionStorage.getItem("idUser"), "montant": document.getElementById("montantdonne").value } }).then((res) => {
-      console.log(res.data); 
-    if (res.data["Compte"] != null) {
+      console.log(res.data);
+      if (res.data["Compte"] != null) {
         setErreur(res.data["Compte"]);
       } else {
         setRencherir(false);
@@ -111,6 +114,15 @@ const Accueil = () => {
     })
   }
 
+  //fiche enchere
+  function ficheEnchere(id) {
+    axios.get("http://localhost:4444/ficheenchere/" + id).then((res) => {
+      console.log(res.data);
+      setFiche(res.data);
+      setVoirFiche(true);
+    })
+
+  }
   return (
     <div id="container">
       <header>
@@ -153,9 +165,11 @@ const Accueil = () => {
                         }
                         {/* <Card.Img variant="top" src="holder.js/100px160" /> */}
                         <Card.Body>
-                          <Card.Title>{value.libelle} </Card.Title>
+                          <Card.Title onClick={() => ficheEnchere(value.idEnchere)} style={{ cursor: 'pointer' }}>{value.libelle} </Card.Title>
                           <Card.Text>
                             Prix :{value.montant} Ar
+                            <br />
+                            Fin:{value.dateFin}
                           </Card.Text>
                         </Card.Body>
                         <Card.Footer>
@@ -224,8 +238,7 @@ const Accueil = () => {
         </div>
 
         <div className="text-center text-dark p-3" >
-          © ETU
-          <a className="text-dark" href="https://mdbootstrap.com/"> 1502,1391</a>
+          <p> 1502,1391</p>
         </div>
       </footer>
 
@@ -250,6 +263,83 @@ const Accueil = () => {
               ''
           }
         </Modal.Body>
+      </Modal>
+
+      {/* modal fiche */}
+      <Modal show={voirfiche} onHide={() => setVoirFiche(false)}>
+      <Icon.XSquare style={{cursor:'pointer'}} onClick={()=>setVoirFiche(false)}/>
+          {
+            fiche != null ?
+              fiche["fiche"].map((value) => {
+                return (
+                    <MDBContainer key={value.idEnchere} className="py-5 h-100" >
+                      <MDBRow>
+                        <MDBCol xl="12">
+                          <MDBCard>
+                            <div className="rounded-top text-white d-flex flex-row" style={{ backgroundColor: '#000', height: '200px' }}>
+                              <div className="ms-4 mt-5 d-flex flex-column" style={{ width: '150px' }}>
+                                <MDBCardImage src={imgvente}
+                                  alt="imgvente" className="mt-4 mb-2 img-thumbnail" fluid style={{ width: '150px', zIndex: '1' }} />
+                              </div>
+                              <div className="ms-3" style={{ marginTop: '130px' }}>
+                                <MDBTypography tag="h5">{value.libelle}</MDBTypography>
+                                <MDBCardText>Categorie: {value.categorie}</MDBCardText>
+                              </div>
+                            </div>
+                            <div className="p-4 text-black" style={{ backgroundColor: '#f8f9fa' }}>
+                              <div className="d-flex justify-content-end text-center py-1">
+                                <div>
+                                  <MDBCardText className="mb-1 h5">{value.montant}</MDBCardText>
+                                  <MDBCardText className="small text-muted mb-0">Prix</MDBCardText>
+                                </div>
+                                <div className="px-3">
+                                  <MDBCardText className="mb-1 h5">{value.statut}</MDBCardText>
+                                  <MDBCardText className="small text-muted mb-0">Statut</MDBCardText>
+                                </div>
+                              </div>
+                            </div>
+                            <MDBCardBody className="text-black p-4">
+                              <div className="mb-5">
+                                <p className="lead fw-normal mb-1">About</p>
+                                <div className="p-4" style={{ backgroundColor: '#f8f9fa' }}>
+                                  <MDBCardText className="font-italic mb-1">Produit: {value.produitEnchere}</MDBCardText>
+                                  <MDBCardText className="font-italic mb-1">Date de publication: {value.dateHeure}</MDBCardText>
+                                  <MDBCardText className="font-italic mb-0">Duree: {value.duree} jour</MDBCardText>
+                                </div>
+                              </div>
+                              <div className="d-flex justify-content-between align-items-center mb-4">
+                                <MDBCardText className="lead fw-normal mb-0">Photos</MDBCardText>
+                              </div>
+                              <MDBRow>
+                                <MDBCol className="mb-2">
+                                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
+                                    alt="image 1" className="w-100 rounded-3" />
+                                </MDBCol>
+                                <MDBCol className="mb-2">
+                                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
+                                    alt="image 1" className="w-100 rounded-3" />
+                                </MDBCol>
+                              </MDBRow>
+                              <MDBRow className="g-2">
+                                <MDBCol className="mb-2">
+                                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
+                                    alt="image 1" className="w-100 rounded-3" />
+                                </MDBCol>
+                                <MDBCol className="mb-2">
+                                  <MDBCardImage src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
+                                    alt="image 1" className="w-100 rounded-3" />
+                                </MDBCol>
+                              </MDBRow>
+                            </MDBCardBody>
+                          </MDBCard>
+                        </MDBCol>
+                      </MDBRow>
+                    </MDBContainer>
+                );
+              })
+              :
+              ''
+          }
       </Modal>
 
       {/* modal connexion */}
